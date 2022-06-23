420ce70a22682aac9f5b78555b32cf9a1d8580bd
Zakaria <zaktakk96@gmail.com>
2022-06-23 23:32:55 +0300

    Backend - Created Contact model

--- backend-nodejs/index.js
+++ backend-nodejs/index.js
@@ -3,10 +3,11 @@ require("../backend-nodejs/connection");
 const express = require("express"); // import express server
 const port = 3000;
 const mongoose = require("mongoose");
+const User = require("./Models/User");
 
 const app = express(); // create app by initializing express server
 
-app.use(express.json);
+app.use(express.json());
 
 app.get("/", function (request, response) {
   response.send("Hello World");
