16d717b4c8ae635e5109678e65423155ae573346
Zakaria <zaktakk96@gmail.com>
2022-06-25 20:52:04 +0300

    Frontend - Implemented "Delete Contact"

--- frontend-react/src/components/Contacts/Contact.jsx
+++ frontend-react/src/components/Contacts/Contact.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
 
 const Contact = ({contact_data}) => {
+
+  // Delete contact (passing contact id)
+  async function deleteContact(id) {
+    const res = await fetch(`http://localhost:8080/api/contact/?id=${id}`, {
+      method: "DELETE",
+    });
+    if (res.status === 200) {
+      console.log("success")
+    }
+    else {
+      console.log("Error")
+    }
+    //Checking Deletion Status
+    // res.status === 200
+    //   ? setTasks(contacts.filter((contact) => contact.id !== id))
+    //   : alert("Error Deleting");
+  };
+
   return (
     <tr>
       <td>{contact_data.name}</td>
@@ -9,7 +27,7 @@ const Contact = ({contact_data}) => {
       <td>{contact_data.relationship_status}</td>
       <td>location</td>
       <td>Edit</td>
-      <td>Delete</td>
+      <td><button onClick={ () => deleteContact(contact_data._id)}>Delete</button></td>
     </tr>
   )
 }
