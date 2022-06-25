fbecc722ab7a35587d654da33365a98f878ce89a
Zakaria <zaktakk96@gmail.com>
2022-06-25 15:14:11 +0300

    Backend - Added comments for easier frontend fetching data

--- backend-nodejs/routes.js
+++ backend-nodejs/routes.js
@@ -15,9 +15,9 @@ router.post("/auth/register", register);
 router.post("/auth/login", login);
 
 // Contacts
-router.post("/contact", addContact);
-router.delete("/contact", removeContact);
-router.get("/contacts", getUserContacts);
-router.put("/contact", updateContact);
+router.post("/contact", addContact); // pass body params
+router.delete("/contact", removeContact); // pass the contact id in the URL (?id=...)
+router.put("/contact", updateContact); // update a certain contact, pass the id of contact in the url (?id=...), pass other params in the body
+router.get("/contacts", getUserContacts); // get all contacts of a certian user, pass the user_id in the url (?user_id=...)
 
 module.exports = router;
