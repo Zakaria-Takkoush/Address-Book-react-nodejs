d3225a00dbe434adf98c1bc8a9871b300228b5d8
Zakaria <zaktakk96@gmail.com>
2022-06-27 12:11:18 +0300

    Frontend - Created DisplayMap component

---
+++ frontend-react/src/components/Contacts/DisplayMap.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {useState} from "react";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from "leaflet";
+import * as parkData from "./data/skateboard-parks.json";
+
+const DisplayMap = () => {
+  return (
+    <div>DisplayMap</div>
+  )
+}
+
+export default DisplayMap
